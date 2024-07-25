9979470ccc6f947079c276c22ed0fbecd40dce54
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-07-25 10:31:36 +0700

    Create main.py

---
+++ Episode 21 - If dan Else statement/main.py
@@ -0,0 +1,24 @@
+# if dan else statement
+
+# # if 'kondisinya'
+#     'aksinya'
+
+input_nama = input("Siapa nama anda??\njawab:")
+
+# 1. program if inline
+# if input_nama == "makhasin" : print("kamu ganteng bgttt")
+
+# 2. program if indentation
+# if input_nama == "makhasin":
+#     print("kamu gantenggg bgtt njirr")
+#     print("kamu juga keren abieezzz")
+# print(f"terimakasih {input_nama}")
+
+# 3. program else statement
+if input_nama == "shella":
+    print("kamu cantik bangetttzzzz")
+    print("kamu juga lucuuu abieezzz")
+else:
+    print("kamu siapa yee ga kenall")
+
+print("thanks")
