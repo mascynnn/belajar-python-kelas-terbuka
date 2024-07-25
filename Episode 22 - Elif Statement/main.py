84bbea50f657b1b0ba9c14a55db171da8621cc01
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-07-25 11:05:22 +0700

    Create main.py

---
+++ Episode 22 - Elif Statement/main.py
@@ -0,0 +1,16 @@
+# elif statement (else if)
+
+input_nama = input("siapa namamu??\njawab: ")
+
+if input_nama == "shella" or input_nama == "shela" or input_nama == "sela": # opsi 1
+    print("kamu cantikkk bingittss pokonyaa, gada tandingan")
+elif input_nama == "dya": # opsi 2
+    print("kamu maniss bingittss, gula aja kalah")
+elif input_nama == "ayu": # opsi 3
+    print("kamu kek namamuuu, ayuu tenannn")
+elif input_nama == "pratiwi" or input_nama == "tiwi": # opsi 4
+    print("kamu lucuu bingitttss pen nyubittt aku")
+else: # opsi selain 4 opsi diatas
+    print("kamu siapa yeee ga kenall")
+
+print("tengkyuuu")
