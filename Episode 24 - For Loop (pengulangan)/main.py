94abcadcb2794326a012f199d5c6cf22bfde7925
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-07-26 21:34:38 +0700

    Create main.py

---
+++ Episode 24 - For Loop (pengulangan)/main.py
@@ -0,0 +1,28 @@
+# perulangan (loop)
+
+# for kondisi
+# aksi
+
+number_1 = [0,2,4,6,8]
+print(number_1)
+
+for i in number_1:
+    print(f"i sekarang -> {i}")
+
+print("end of program 1......\n")
+
+# ini dengan range
+number_range = range(1,10)
+
+for i in number_range:
+    print(f"i sekarang -> {i}")
+
+print("end of program 2......\n")
+
+# menggunakan string
+string_1 = "shella is very very beautiful"
+
+for char in string_1:
+    print(char)
+
+print("end of program 3.......\n")
