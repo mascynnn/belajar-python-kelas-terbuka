88e2f5a5a13eb0ef0ea05865244dda300bbb69b3
Makhasin Muhammad <167407468+mascynnn@users.noreply.github.com>
2024-07-26 21:36:33 +0700

    Create main.py

---
+++ Episode 25 - While Loop (pengulangan)/main.py
@@ -0,0 +1,14 @@
+# while loop
+
+# while condition:
+#     action
+#     action
+# end program
+
+number = 0
+print(f"\nCurrent number -> {number}")
+
+while number < 5:
+    number += 1
+    print(f"Now number -> {number}")
+print("Enough....\n")
